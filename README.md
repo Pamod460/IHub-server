2cb1a05fe298268bcb6b6231cce6d93b9ad2deb8
Pamod460 <pamodkaushika@gmail.com>
2022-10-14 23:42:17 +0530

    initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# IHub-server
- 
---
+++ src/main/java/com/example/IHubserver/IHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.IHubserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHubServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHubServerApplication.class, args);
+	}
+
+}
